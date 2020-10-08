26c6587aca9ed733821dcc6968c4a77d3fdc9aca
Jayden Lee <Jayden@rlc.com.au>
2020-10-08 20:36:47 +1100

    Added Notes
    If we want to create a rest framework with django, we will need to use django rest framework. Like flask, but makes your life a lot easier and will integrate in with django models. Will integrate it in later.

--- src/server/inventory/views.py
+++ src/server/inventory/views.py
@@ -1,6 +1,18 @@
+from django.views import generic, View
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.urls import get_resolver
+from .models import Author
+from django.core.serializers.json import DjangoJSONEncoder
+
+### Probably need to use django rest framework as a new pip framework. 
+
 
 # Create your views here.
+
+
+def returnAuthor(request):
+    # authors = 
 
 
 ## HTTP STUFF
