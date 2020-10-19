1442e0d4b5dfa80ac639d98165e14b14b9cc04ce
Albert Ferguson <albertferguson118@gmail.com>
2020-10-19 23:55:22 +1100

    updated urls and views in elasticsearch-app.py with options HTTP method, config and removed MORE redundant imports.

--- src/server/elasticsearch-app/views.py
+++ src/server/elasticsearch-app/views.py
@@ -1,30 +1,16 @@
 # views and mixins
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView, View
-
-# authentication and session
-from django.contrib.auth import authenticate, login, logout  # Auth built in functions
-from django.contrib.auth import update_session_auth_hash # update for session 
-from django.contrib import messages
-
-# forms and more builtins
-from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.views.generic import View
 
 # Http req and res
-from django.http import HttpRequest, Http404, HttpResponse, HttpResponseBadRequest
+from django.http import HttpRequest
+
 # csrf and api logic
 from django.http import JsonResponse
-from django.middleware.csrf import get_token
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
-import json # for serialization and deserialization
 
-from django.shortcuts import render
-from django.urls import get_resolver
-from django.utils import timezone
-
+# elastic search and abstract syntax tree libs
 from elasticsearch import Elasticsearch
 import ast
 
@@ -36,6 +22,9 @@ class Recipe(View):
     
     Recipe views via Elasticsearch. The Django Proxy implementation.
     """
+
+    allowed_methods = ["get", "options"]
+
     def __init__(self, *args, **kwargs):
         # default config
         ip_default = "127.0.0.1"
@@ -73,6 +62,16 @@ class Recipe(View):
         recipe_search = ast.literal_eval(recipe_search)
         return JsonResponse(recipe_search, status = 200)
 
+    def options(self, request, *args, **kwargs):
+        """
+        Author: Albert Ferguson
+        
+        Returns the options allowed for the current view.
+        """
+
+        response = HttpResponse()
+        response['allow'] = ','.join(self.allowed_methods)
+        return response
 
     def _buildRecipesQuery(inventoryList, keywords=None, *args, **kwargs):
         """
